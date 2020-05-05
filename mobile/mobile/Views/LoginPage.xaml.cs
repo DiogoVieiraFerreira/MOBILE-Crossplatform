a9ecc26d8753bab02ddea55e585c3e96431b0883
DiogoVF <diogo.vieira-ferreira@cpnv.ch>
2020-05-05 14:38:13 +0200

    Add project files.

---
+++ mobile/mobile/Views/LoginPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace mobile.Views
+{
+    public partial class LoginPage : ContentPage
+    {
+        string _fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "token");
+        public LoginPage()
+        {
+            InitializeComponent();
+            if (File.Exists(_fileName))
+            {
+                string token = File.ReadAllText(_fileName);
+            }
+        }
+
+        private void Button_Register(object sender, EventArgs e)
+        {
+            Console.WriteLine(FirstName.Text);
+            Console.Error.WriteLine(LastName.Text);
+            Console.WriteLine(mobile.Text);
+        }
+
+        private void Button_Login(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
