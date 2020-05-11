12a66abcde1397b1989ebd19f14301a92c55a808
DiogoVF <diogo.vieira-ferreira@cpnv.ch>
2020-05-11 13:56:02 +0200

    create user model

---
+++ mobile/mobile/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mobile.Models
+{
+    class User
+    {
+        public string FirstName { get; protected set; }
+        public string LastName { get; protected set; }
+        public string PhoneNumber{ get; protected set; }
+        public string Token { get; protected set; }
+
+        public User(string firstname, string lastname, string phonenumber)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.PhoneNumber = phonenumber;
+        }
+
+        public User(string token)
+        {
+            this.Token = token;
+        }
+    }
+}
