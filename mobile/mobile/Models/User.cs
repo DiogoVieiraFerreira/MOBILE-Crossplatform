92fb582279eaccbb2bbd39f519d30a51b94ebf2c
DiogoVF <diogo.vieira-ferreira@cpnv.ch>
2020-05-15 20:40:26 +0200

    Connection with api to sign in and sign up and save token on file

--- mobile/mobile/Models/User.cs
+++ mobile/mobile/Models/User.cs
@@ -6,16 +6,19 @@ namespace mobile.Models
 {
     class User
     {
+        public int ID { get; set; }
         public string Firstname { get; protected set; }
         public string Lastname { get; protected set; }
-        public string PhoneNumber{ get; protected set; }
+        public string PhoneNumber{ get; set; }
         public string Token { get; protected set; }
-
-        public User(string firstname, string lastname, string phoneNumber)
+        public User(string token)
+        {
+            this.Token = token;
+        }
+        public User(string firstname, string lastname)
         {
             this.Firstname = firstname;
             this.Lastname = lastname;
-            this.PhoneNumber = phoneNumber;
         }
 
         public static bool CheckInformations(string firstname, string lastname, string phoneNumber)
@@ -26,9 +29,5 @@ namespace mobile.Models
             return true;
         }
 
-        public User(string token)
-        {
-            this.Token = token;
-        }
     }
 }
