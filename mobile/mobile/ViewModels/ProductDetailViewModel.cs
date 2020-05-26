cc82b096789c08a1190afb86f3602a83ff5566e0
DiogoVF <diogo.vieira-ferreira@cpnv.ch>
2020-05-26 17:23:19 +0200

    Start Product Detail

---
+++ mobile/mobile/ViewModels/ProductDetailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Android.Icu.Lang;
+using mobile.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mobile.ViewModels
+{
+    public class ProductDetailViewModel : BaseViewModel
+    {
+        public Product Product { get; set; }
+        public ProductDetailViewModel(Product product = null)
+        {
+            Title = product?.Name;
+            this.Product = product;
+        }
+    }
+}
---
+++ mobile/mobile/Views/ProductDetailPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using mobile.Models;
+using mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductDetailPage : ContentPage
+    {
+        ProductDetailViewModel viewModel;
+        public ProductDetailPage(ProductDetailViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext  = this.viewModel = viewModel;
+        }
+        public ProductDetailPage()
+        {
+            InitializeComponent();
+
+            var product = new Product
+            {
+                Name = "Item 1",
+                Details = "This is an item description."
+            };
+
+            viewModel = new ProductDetailViewModel(product);
+            BindingContext = viewModel;
+        }
+    }
+}
